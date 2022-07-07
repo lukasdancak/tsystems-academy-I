cfdd7cea52ae3be026a8656d6fece67e294bfbe4
Lukas_Dancak <lukas.dancak@gmail.com>
2022-07-07 13:52:36 +0200

    Pridane "Nazdar"

--- src/HelloWorld2.java
+++ src/HelloWorld2.java
@@ -1,9 +1,9 @@
 public class HelloWorld2 {
     public static void main(String[] args) {
 
-        for (int i=0; i<3; i++){ System.out.println("Hello, World!"); }
+        for (int i=0; i<3; i++){ System.out.println("Nazdar, World!"); }
         int x=0;
-        while(x<3) {System.out.println("Hello, World!");x++;}
+        while(x<3) {System.out.println("Nazdar, World!");x++;} //pridane "Nazdar"
 		
     }
 }
