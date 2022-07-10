973a7cb2b1c1aa9415b5733ff13d9146e698a6dd
lukasdancak <lukas.dancak@gmail.com>
2022-07-11 00:39:37 +0200

    uloha Bubble Sort urobena

--- Arrays/ArrayCopyDemoDomaca.java
+++ Arrays/ArrayCopyDemoDomaca.java
@@ -4,9 +4,11 @@ public class ArrayCopyDemoDomaca {
         int[] copyTo = new int[7];
         int[] copyFrom2 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 133, 14};
         int[] copyTo2 = new int[7];
+        int[] random1 = {1, 12, 7, 4, 21, 6, 55, 8, 4, 10, 33, 12, 133, 14};
 
 
-        ProcessTimer test1 = new ProcessTimer("Javovsky array copy"); // start merania casu
+
+ /*       ProcessTimer test1 = new ProcessTimer("Javovsky array copy"); // start merania casu
         test1.startTimer();
 
         System.arraycopy(copyFrom, 2, copyTo, 0, 7); // systemovy JAVA array copy
@@ -29,6 +31,65 @@ public class ArrayCopyDemoDomaca {
             System.out.print(i + ",");
         }
         System.out.println();
+        */
+
+//*************** uloha Bubble Sort *********************
+
+        for (int i : random1) {                  //vypis array
+            System.out.print(i + ",");          //
+        }                                       //
+        System.out.println();                   //
+
+        bubbleSort(random1); // bubble sort funkcia
+
+        for (int i : random1) {                  //vypis array
+            System.out.print(i + ",");           //
+        }                                        //
+        System.out.println();                    //
+
+//*************** uloha Bubble Sort *********************
+
+    }
+
+    // bubble sort
+    private static void bubbleSort(int[] array) {
+        int limit = array.length;
+        int nextLimit = 0;
+        boolean noSwap = false;
+        while (limit > 1) {
+            for (int j = 0; j < limit - 1; j++) {
+                if (swapArraysCells(array, j)) {
+                    noSwap = false;
+                } else {
+                    if (noSwap == false) {
+                        noSwap = true;
+                        nextLimit = j + 1;
+                    }
+                }
+            }
+            if (noSwap == true) {
+                limit = nextLimit;
+                noSwap = false;
+            } else {
+                limit = limit - 1;
+            }
+
+
+        }
+
+    }
+
+    // metoda vymeni dva susedne bunky ak cislo v lavej je vacsie ako v pravej
+    // pri pouziti pozor na null pointer exception, nie je riesena
+    private static boolean swapArraysCells(int[] array, int i) {
+        if (array[i] > array[i + 1]) {
+            int temp = 0;
+            temp = array[i];
+            array[i] = array[i + 1];
+            array[i + 1] = temp;
+            return true;
+        }
+        return false;
     }
 
 
