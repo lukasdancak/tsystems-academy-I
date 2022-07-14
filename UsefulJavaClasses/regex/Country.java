8c76814637f2f4210ee330e756b71449fbcc61f0
lukasdancak <lukas.dancak@gmail.com>
2022-07-14 15:22:36 +0200

    trieda Counrties + regex

---
+++ UsefulJavaClasses/regex/Country.java
@@ -0,0 +1,11 @@
+package regex;
+
+public class Country {
+    String name;
+    String shortcut;
+
+    public Country(String name, String shortcut) {
+        this.name = name;
+        this.shortcut = shortcut;
+    }
+}
