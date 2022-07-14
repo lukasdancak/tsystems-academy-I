e86e75c2fa2d32fac208d95cbc087a4c7415eef4
lukasdancak <84349872+lukasdancak@users.noreply.github.com>
2022-07-14 12:53:44 +0200

    Update ArrayCopyDemo.java

--- Arrays/ArrayCopyDemo.java
+++ Arrays/ArrayCopyDemo.java
@@ -1,6 +1,6 @@
 public class ArrayCopyDemo {
     public static void main(String[] args) {
-        char[] copyFrom = { 'd', 'e', 'c', 'a', 'f', 'f', 'e',
+        char[] copyFromjhvkjhvjhvhj = { 'd', 'e', 'c', 'a', 'f', 'f', 'e',
 			    'i', 'n', 'a', 't', 'e', 'd' };
         char[] copyTo = new char[7];
 
